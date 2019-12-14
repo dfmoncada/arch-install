32339c0e8dcf22b0fee1cc1a2f94dd0147ed51c0
Diego Moncada <dfmoncada@gmail.com>
2019-12-13 21:18:48 -0300

    copy file into file directory

--- arch-install.sh
+++ arch-install.sh
@@ -30,30 +30,32 @@ pacstrap /mnt base base-devel linux linux-firmware vim grub efibootmgr sudo
 
 # create fstab
 genfstab -U /mnt >> /mnt/etc/fstab
+cp ./arch-install.sh /mnt/
 
 arch-chroot /mnt
 
-## IN ARCH new system AS ROOT
-## NEW SHELL SCRIPT 
-ln -sf /usr/share/zoneinfo/Chile/Continental /etc/locale
-hwclock --systohc
 
-# language 
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-localge-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-
-# skipping network conf since I believe is not crucial at the time
-
-# create initramfs. usually runs at pacstrap
-# mkinitcpio -P 
-
-# change root pass
-passwd
-useradd -mg users -G wheel,storage,power -s /bin/bash diego
-visudo 
-passwd diego
-
-# boot loader
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+# ## IN ARCH new system AS ROOT
+# ## NEW SHELL SCRIPT 
+# ln -sf /usr/share/zoneinfo/Chile/Continental /etc/locale
+# hwclock --systohc
+# 
+# # language 
+# echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+# localge-gen
+# echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+# 
+# # skipping network conf since I believe is not crucial at the time
+# 
+# # create initramfs. usually runs at pacstrap
+# # mkinitcpio -P 
+# 
+# # change root pass
+# passwd
+# useradd -mg users -G wheel,storage,power -s /bin/bash diego
+# visudo 
+# passwd diego
+# 
+# # boot loader
+# grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+# grub-mkconfig -o /boot/grub/grub.cfg
